a4b49b3e6570aacbd51b7a73c21cc7fffd9cca9d
Ktron <martinjuandiazmarin@gmail.com>
2026-05-19 12:44:38 -0500

    Inicializando React

---
+++ REACT/basketball-dashboard/src/App.jsx
@@ -0,0 +1,21 @@
+function App(){
+
+  return(
+
+    <main className="dashboard">
+
+      <section className="dashboard__container">
+
+        <h1>
+          React funcionando 🚀
+        </h1>
+
+      </section>
+
+    </main>
+
+  )
+
+}
+
+export default App
